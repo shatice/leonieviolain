2982959857d612e2c04a836d4d9f5c91a5294ff8
shatice <sarah.gungor@hetic.net>
2020-03-12 10:33:37 +0100

    add hover animation to HP

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 /***** ROUTER *****/
 import { BrowserRouter as Router, Link } from "react-router-dom";
@@ -13,18 +13,41 @@ import moviesLink from '../../datas/moviesLink';
 import Header from '../header/Header';
 
 const Home = () => {
+	const [ isHovered, setIsHovered ] = useState(false);
+
+	/**
+	 * When hoverring a movie title, body's bg-image === title's bg-image
+	 */
+	const toggle = ( id ) => {
+		if ( isHovered ) {
+			setIsHovered(false); 
+			document.body.classList.remove( 'is-changed-' + id );
+		} else {
+			setIsHovered(true);
+			document.body.classList.add( 'is-changed-' + id );
+		}
+	}
+
 	return (
 		<div id ="home" className="home">
 			<Header/>
+
 			<ul>
-				{ moviesLink ? moviesLink.map(c => 
-				<li className={ "movie movie--" + ( c.id ) } key={ c.id }>
-						<Link id={ c.id } className="movieTitle" to="/movie">
-							<p>{ c.title }</p>
-							{ c.titleBis ? <p>{ c.titleBis }</p> : ""}
-						</Link>
-				</li>
-				) : "" }
+				{ moviesLink ? moviesLink.map( c => <li className={ "movie movie--" + ( c.id ) } key={ c.id }>
+
+					<Link 
+					id={ c.id } 
+					className="movieTitle" 
+					to="/movie" 
+					onMouseEnter={() => { toggle( c.id ) }}
+					onMouseLeave={() => {  toggle( c.id ) }}>
+
+						<p>{ c.title }</p>
+						{ c.titleBis ? <p>{ c.titleBis }</p> : ""}
+
+					</Link>
+
+				</li> ) : "" }
 			</ul>
 		</div>
 	);
